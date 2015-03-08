f0502e969dcceff40f20eb49bc217db801d15f88
Kasra Rahjerdi <krahjerdi@stackoverflow.com>
2015-03-08 16:18:05 -0400

    Ok Flask-Funnel was a bad idea. To Flask-Assets!

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import os
 from flask import Flask, render_template
-from flask_funnel import Funnel
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
-app.debug = True
-Funnel(app)
-app.config['LESS_PREPROCESS'] = True
+app.debug = True if os.getenv("ENV", "DEBUG") is "PROD" else False
+assets = Environment(app)
 
-app.config['CSS_BUNDLES'] = {
-    'index': (
-        '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/default.min.css',
-        'css/index.less',
-    ),
-}
+js_base = Bundle('external/jquery.min.js',
+            'external/highlight/highlight.min.js',
+            filters='jsmin', output='gen/base.js')
+js_index = Bundle('js/index.js', 'js/markdown.min.js',
+            filters='jsmin', output='gen/index.js')
 
-app.config['JS_BUNDLES'] = {
-    'index': (
-        '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
-        '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js',
-        'js/markdown.min.js',
-        'js/index.js',
-    ),
-}
+assets.register('js_base', js_base)
+assets.register('js_index', js_index)
+
+css_index = Bundle('external/highlight/default.min.css',
+                Bundle('css/index.less', filters='less'),
+                filters='cssmin', output='gen/index.css')
+assets.register('css_index', css_index)
 
 @app.route('/')
 def home():
