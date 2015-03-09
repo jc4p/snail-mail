6a84eebcd5b4cf19e68e5f47da0ed0754197cf55
Kasra Rahjerdi <krahjerdi@stackoverflow.com>
2015-03-09 02:48:31 -0400

    Last commit for the night.
    JSPDF doesn't do styling so it's a no-go all together.
    Html2Canvas is super unstable, 0.4 generates completely blank (but correct
    sized) images, 0.5/master generate great look PNGs except their
    image-proxy logic is broken (it tries to use CORS but doesn't)
    
    Looks like we're gonna have go back to the "Send HTML to site" --> "Use
    phantom.js or something to make an image" idea. Oh well. Would've been
    nice to be client side only.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, Response
 from flask.ext.assets import Environment, Bundle
+from html2canvasproxy import *
 
 app = Flask(__name__)
-app.debug = True #True if os.getenv("ENV", "DEBUG") is "PROD" else False
+app.config['DEBUG'] = True #True if os.getenv("ENV", "DEBUG") is "PROD" else False
 assets = Environment(app)
 
 js_base = Bundle('external/jquery.min.js',
@@ -11,6 +13,7 @@ js_base = Bundle('external/jquery.min.js',
             'external/flat-ui/flat-ui-pro.min.js',
             filters='jsmin', output='gen/base.js')
 js_index = Bundle('js/index.js', 'js/markdown.min.js',
+            'external/html2canvas-0.4.1.js',
             output='gen/index.js')
 
 assets.register('js_base', js_base)
@@ -27,6 +30,70 @@ css_index = Bundle('external/highlight/default.min.css',
 assets.register('css_base', css_base)
 assets.register('css_index', css_index)
 
+h2c = None
+real_path = os.path.dirname(os.path.realpath(__file__)) + '/tmp'
+virtual_path = '/html2canvas/static'
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/send-letter', methods=['GET', 'POST'])
+def send():
+    data = request.form
+
+    sender = {'name': data['from-name'], 'address': data['from-address'], 'city': data['from-location'] }
+    recipient = {'name': data['to-name'], 'address': data['to-address'], 'city': data['to-location'] }
+
+    createdObject = createLobObject(data['html'])
+    raise ValueError(createdObject)
+
+
+def createLobObject(html):
+    payload = {'setting': 100, 'template': 1}
+    auth = ('test_07fa45ae745b1d90e49e36ebb2112d6c128', '')
+
+    tempFile = TemporaryFile()
+    tempFile.write(html)
+
+    files = {"file": ('upload.html', html, 'text/html')}
+ 
+    res = requests.post('https://api.lob.com/v1/objects', data=payload, auth=auth, files=files)
+    return res.json()
+
+
+@app.route('/html2canvas-proxy')
+def html2canvas_proxy():
+    h2c = html2canvasproxy(request.args.get('callback'), request.args.get('url'))
+
+    h2c.enableCrossDomain()
+
+    h2c.userAgent(request.headers['user_agent'])
+    h2c.hostName(request.url)
+
+    if request.referrer is not None:
+        h2c.referer(request.referrer)
+
+    h2c.route(real_path, virtual_path)
+
+    if request.args.get('debug_vars'):
+        return Response(str(h2c.debug_vars()), mimetype='text/plain')
+
+    r = h2c.result()
+
+    return Response(r['data'], mimetype=r['mime'])
+
+
+@app.route('/html2canvas/static/<image>')
+def images(image):
+    res = html2canvasproxy.resource(real_path, image)
+
+    if res is None:
+        return '', 404
+    else:
+        return Response(res['data'], mimetype=res['mime'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
