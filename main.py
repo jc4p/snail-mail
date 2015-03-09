585849431faf2554aa83bd346128b8095fac10d6
Kasra Rahjerdi <krahjerdi@stackoverflow.com>
2015-03-08 22:13:11 -0400

    Proof of concept
    Doesn't actually charge money, or send things to Lob yet -- All it does is
    open up a pop-up (make sure your pop-up blocker doesn't block it, if it
    does tell it to always accept then refresh) after you do a fake
    transcation (either by using the Stripe test credit card codes or by going
    to the Bitcoin tab and waiting 10 seconds) and show you what the actual
    HTML being sent over to Lob.com would look like.

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ from flask import Flask, render_template
 from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
-app.debug = True if os.getenv("ENV", "DEBUG") is "PROD" else False
+app.debug = True #True if os.getenv("ENV", "DEBUG") is "PROD" else False
 assets = Environment(app)
 
 js_base = Bundle('external/jquery.min.js',
             'external/highlight/highlight.min.js',
+            'external/flat-ui/flat-ui-pro.min.js',
             filters='jsmin', output='gen/base.js')
 js_index = Bundle('js/index.js', 'js/markdown.min.js',
-            filters='jsmin', output='gen/index.js')
+            output='gen/index.js')
 
 assets.register('js_base', js_base)
 assets.register('js_index', js_index)
 
+css_base = Bundle('external/bootstrap.min.css',
+                'external/flat-ui/flat-ui-pro.min.css',
+                filters='cssmin', output='gen/base.css')
+
 css_index = Bundle('external/highlight/default.min.css',
                 Bundle('css/index.less', filters='less'),
                 filters='cssmin', output='gen/index.css')
+
+assets.register('css_base', css_base)
 assets.register('css_index', css_index)
 
 @app.route('/')
--- static/js/index.js
+++ static/js/index.js
@@ -13,9 +13,123 @@ $(document).ready(function() {
         input.editor = this;
         this.update();
     }
-    var editor = new Editor($("#message-text")[0], $("#letter-preview")[0]);
+    var editor = new Editor($("#message-text")[0], $("#message-contents")[0]);
 
     $("#message-text").on('input', function(e) {
         editor.update();
     });
+
+    // We should also update the From/To fields when they change them.
+    $(".from-container input").on('input', function(e) {
+        updateFrom();
+    });
+
+    $(".recipient-container input").on('input', function(e) {
+        updateTo();
+    });
+
+    function updateFrom() {
+        name = valOrDefault($("#from-name"), "Your Name");
+        address = valOrDefault($("#from-address"), "Your Address");
+        city = valOrDefault($("#from-location"), "Your City And State");
+        updateViaInput($("#return-address-text"), name, address, city);
+
+        // And do the image too
+        imagePath = valOrDefault($("#from-image"), "");
+        if (imagePath) {
+            $("#return-address-image img").prop("src", imagePath);
+            $("#return-address-image").show();
+        } else {
+            $("#return-address-image").hide();
+        }
+    }
+
+    function updateTo() {
+        name = valOrDefault($("#to-name"), "Their Name");
+        address = valOrDefault($("#to-address"), "Their Address");
+        city = valOrDefault($("#to-location"), "Their City And State");
+        updateViaInput($("#recipient-address-text"), name, address, city);
+    }
+
+    function updateViaInput(textContainer, name, address, city) {
+        textContainer.text(name)
+        textContainer.append("<br>");
+        textContainer.append(document.createTextNode(address));
+        textContainer.append("<br>");
+        textContainer.append(document.createTextNode(city));
+    }
+
+    function valOrDefault(elem, def) {
+        return elem.val() ? elem.val() : def;
+    }
+
+    function sendLetter(tokenId) {
+        data = $("#main-form").serializeObject();
+        data['token'] = tokenId;
+        html = '<head>' + BASE_CSS + PRINT_CSS + '</head>';
+        html += $(".letter-preview").html();
+        data['html'] = html;
+
+        var win = window.open("", "Print Preview");
+        win.document.write(html);
+
+        return;
+        $.post("/send-letter", data, function(res) {
+            console.log(res);
+        });
+    }
+
+    $("#payment-btn-twitter").on('click', function(e) {
+        //sendLetter(1);
+        e.preventDefault();
+    });
+
+    // Init stuff
+    $("select").select2();
+    $(".letter-preview").animate({scrollTop: $(".letter-preview")[0].scrollHeight});
+
+    // Stripe stuff
+    var handler = StripeCheckout.configure({
+        key: 'pk_CTZI5qSX6WZy42tRUVkfOsYwfEQQo',
+        token: function(token) {
+            if (!token.hasOwnProperty('id')) {
+                return;
+            }
+
+            sendLetter(token.id);
+        }
+    });
+
+    $('#payment-btn-stripe').on('click', function(e) {
+        // Open Checkout with further options
+        handler.open({
+            name: 'Send Letter',
+            description: '1 Letter ($2.99)',
+            amount: 299,
+            bitcoin: true
+        });
+        e.preventDefault();
+    });
+
+    // Close Checkout on page navigation
+    $(window).on('popstate', function() {
+        handler.close();
+    });
 });
+
+
+$.fn.serializeObject = function () {
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function () {
+       if (o[this.name] !== undefined) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
